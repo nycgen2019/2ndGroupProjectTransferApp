570bc804df018c3ad3ddabb2039b97a5d126745b
nycgen2019 <viviantellez37@gmail.com>
2022-05-30 18:17:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a2ndgroupprojecttransferapp/TransferCreditsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2ndgroupprojecttransferapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransferCreditsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer_credits)
+    }
+}
